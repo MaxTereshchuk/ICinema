702f255d8d331350e4cd54de41ac3b7e455b73fe
k0ntoner <145562858+k0ntoner@users.noreply.github.com>
2024-07-08 21:46:50 +0300

    InitialCreateProject

---
+++ ICinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ICinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
