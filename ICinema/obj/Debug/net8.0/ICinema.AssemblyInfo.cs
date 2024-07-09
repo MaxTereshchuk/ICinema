587c5c9995975b1de9010338456bd5b6b741c393
k0ntoner <145562858+k0ntoner@users.noreply.github.com>
2024-07-09 21:42:34 +0300

    Add new styles to css file

--- ICinema/obj/Debug/net8.0/ICinema.AssemblyInfo.cs
+++ ICinema/obj/Debug/net8.0/ICinema.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICinema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eb256beab07d7322c0df69198a9fb8677064fdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c35262e8cfe8823b6a0a97e9da203217633698b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICinema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICinema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
