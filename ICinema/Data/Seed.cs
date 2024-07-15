16a65945fba5ccae2232af5aed4ad9294f22a2ad
k0ntoner <145562858+k0ntoner@users.noreply.github.com>
2024-07-15 18:10:02 +0300

    Update Seed.cs There were merge conflicts

--- ICinema/Data/Seed.cs
+++ ICinema/Data/Seed.cs
@@ -1,16 +1,5 @@
 ﻿using ICinema.Models;
-<<<<<<< HEAD
-using Microsoft.AspNetCore.Identity;
 
-namespace ICinema.Data
-{
-	public class Seed
-	{
-		
-		
-	}
-}
-=======
 using System.Net;
 
 namespace ICinema.Data
