5e6872309d70312fe232714356e4376d26d37c72
MaxTereshchuk <namestar.max@gmail.com>
2024-07-16 00:13:38 +0300

    Schedule model and gitignore

---
+++ ICinema/Models/Schedule.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ICinema.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Hall { get; set; }
+    }
+}
