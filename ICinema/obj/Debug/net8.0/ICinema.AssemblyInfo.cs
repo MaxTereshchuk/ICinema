75981cd0657b135b6a9898d9e904ea99502f3e61
k0ntoner <145562858+k0ntoner@users.noreply.github.com>
2024-07-15 17:22:03 +0300

    Prepare for merging

--- ICinema/obj/Debug/net8.0/ICinema.AssemblyInfo.cs
+++ ICinema/obj/Debug/net8.0/ICinema.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICinema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5dd741381b07376b99c55b1a6ade7a05db20fcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182969dcdb382bc5ef44cf144ea2689d27d3a798")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICinema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICinema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
